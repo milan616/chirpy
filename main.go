31caba5f7e28156421b98b3952c0dc2a8a873e0d
milan616 <milan616@gmail.com>
2026-07-07 02:10:21 -0400

    Added logo asset

--- main.go
+++ main.go
@@ -8,12 +8,21 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	// 1. Convert the current directory string "." into an http.Dir type
+	directory := http.Dir(".")
+
+	// 2. Pass it to http.FileServer to create a file-serving handler
+	fileServerHandler := http.FileServer(directory)
+
+	// 3. Register it with the ServeMux for the root "/" path
+	mux.Handle("/", fileServerHandler)
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	log.Println("Starting server on port 8080...")
+	log.Println("Starting file server on port 8080...")
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
