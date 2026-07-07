00586f211a0b202c0c4cd5759b63681903dc1704
milan616 <milan616@gmail.com>
2026-07-07 01:57:12 -0400

    basic server, 404 error!

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
-include (
-	"http"
+import (
+	"log"
+	"net/http"
 )
 
+func main() {
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Println("Starting server on port 8080...")
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
